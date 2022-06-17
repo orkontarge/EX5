673b153075d88dbeb64ac5c0731c2e7486640bf5
avigayilBrown <avigayil.wimpfheimer@mail.huji.ac.il>
2022-06-17 14:34:15 +0300

    write until adding 1 to cgropus.proc

--- container.cpp
+++ container.cpp
@@ -15,17 +15,22 @@
 
 #define STACK 8192
 #define PROCESS_ID 1
+#define RELEASE_MODE 1
 #define MODE_MKDIR 0755
 #define ERROR_MSG "system error: %s \n"
 #define PATH_OF_PID "/sys/fs/cgroup/pids"
-#define PROC_FILE "/sys/fs/cgroup/pids/cgroup.procs"
-//TODO check file path, form pid or cgroup?
+#define PROC_PATH "/sys/fs/cgroup/pids/cgroup.procs" //TODO relative or absolute path
+#define PID_PATH "/sys/fs/cgroup/pids.max" //TODO same debate - relative or absolute path.
+#define RELEASE_RESOURCES_PATH "/sys/fs/cgroup/pids/notify_on_release" //TODO where is this located??
 
 using namespace std;
 
 struct ArgsForChild{
     char* new_hostname;
     char* new_filesystem_directory;
+    char* num_processes;
+    char* path_to_program;
+    char* args_for_program[];
 
 };
 void printError(const string& msg){
@@ -47,26 +52,49 @@ int child(const ArgsForChild& args) { //TODO change format for args
     if (chroot(file_directory) == -1){
         printError("problem with new root directory");
     }
+
     //move to the new root directory //TODO: check if this is the right order
     if (chdir(file_directory) == -1){
         printError("problem changing the current working directory");
     }
-    //mount //TODO unmount
-    mount("proc", "/proc", "proc", 0, 0);
 
-    // limit the number of processes
+
+    // limit the number of processes:
     if (mkdir(PATH_OF_PID,MODE_MKDIR) == -1){
         printError("problem with new directory");
     }
 
+    // attach the container process into this new cgroup
     try {
-        ofstream myfile;
-        myfile.open(PROC_FILE);
-        myfile << PROCESS_ID;
-        myfile.close();
+        ofstream file;
+        file.open(PROC_PATH);
+        file << PROCESS_ID;
+        file.close();
     }
     catch(const exception &e){
-        printError("problem with writing to code");
+        printError("problem with proc file access");
+    }
+
+    //set max process number
+    try {
+        ofstream file;
+        file.open(PID_PATH);
+        file << args.num_processes;
+        file.close();
+    }
+    catch(const exception &e){
+        printError("problem with accessing pid file");
+    }
+
+    //mount
+    if (mount("proc", "/proc", "proc", 0, 0) == -1){
+        printError("problem with mounting");
+    }
+
+
+    //run the new program
+    if (execvp(args.path_to_program,args.args_for_program) == -1){
+        printError("problem running new program");
     }
 
     return 0;
@@ -79,16 +107,41 @@ int main(int argc, char* argv[]) {
     if (!stack){
         printError("problem with memory allocation");
     }
-    string new_hostname = argv[0];
-    string new_filesystem_directory = argv[1];
-    string num_processes = argv[2];
-    string path_to_program = argv[3];
-    string args_for_program = argv[4]; //TODO get all program parameters till argc
-    void* args_for_child;
+
+    int size = argc - 3;
+    char* args_for_program = new char[size]; //TODO dealloc
+    for (int i = 3; i < argc; ++i) {
+        args_for_program[i-3] = argv[i]; //TODO problem with type
+    }
+
+    struct ArgsForChild args_for_child{argv[0],argv[1],argv[2],argv[3],args_for_program};
 
     // create new process - will be used as a container
     int child_pid = clone(child, stack+STACK, CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | SIGCHLD
             , args_for_child);
     wait(NULL);
+
+
+    //TODO not sure about the order of the rest of the code. release, umount then delete?
+
+    //release resources of container
+    try {
+        ofstream file;
+        file.open(RELEASE_RESOURCES_PATH);
+        file << RELEASE_MODE;
+        file.close();
+    }
+    catch(const exception &e){
+        printError("problem releasing resource of container");
+    }
+
+
+    //unmount
+    if (umount(PROC_PATH) == -1){ //TODO check if this is the right path
+        printError("problem with mounting");
+    }
+
+    //delete files created for container
+    //TODO files to delete: files - pid.max,cgroup.procs, directories - fs,pids,cgroups
 }
 
