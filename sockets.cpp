0c296a34c219e2b17407f446776b570ae822a32c
avigayilBrown <avigayil.wimpfheimer@mail.huji.ac.il>
2022-06-21 11:49:45 +0300

    write until adding 1 to cgropus.proc

--- sockets.cpp
+++ sockets.cpp
@@ -24,7 +24,7 @@
 #define CLIENT 3
 #define SERVER 2
 #define EXIT_FAILURE 1
-#define BUFFFER_SIZE 256
+#define BUFFER_SIZE 256
 #define ERROR_MSG "system error: "
 #define NULL nullptr
 using namespace std;
@@ -35,9 +35,10 @@ void printError(const string &msg) {
     exit(1);
 }
 
-//TODO THIS IS THE SERVER: socket->bind->listen
+
+//create welcome socket for server
 int establish(unsigned short portnum) {
-    char myname[MAXHOSTNAME+1];
+    char myname[MAXHOSTNAME+1]; //TODO set value
     int s;
     struct sockaddr_in sa;
     struct hostent *hp;
@@ -45,8 +46,10 @@ int establish(unsigned short portnum) {
     //hostnet initialization
     gethostname(myname, MAXHOSTNAME);
     hp = gethostbyname(myname);
-    if (hp == NULL)
-        return(-1);
+    if (hp == NULL){
+        printError("problem with server hostname");
+        exit(EXIT_FAILURE);
+    }
 
     //sockaddrr_in initlization
     memset(&sa, 0, sizeof(struct sockaddr_in));
@@ -58,11 +61,11 @@ int establish(unsigned short portnum) {
 
     //create socket
     if ((s= socket(AF_INET, SOCK_STREAM, 0)) < 0){
-        printError("problem with creating socket");
+        printError("problem with creating server socket");
         exit(EXIT_FAILURE);
     }
 
-    //bind socket to
+    //bind socket: give address to listen on
     if (bind(s , (struct sockaddr *)&sa , sizeof(struct sockaddr_in)) < 0) {
         close(s);
         printError("problem with binding socket");
@@ -76,18 +79,21 @@ int establish(unsigned short portnum) {
     return(s);
 }
 
-//TODO THIS IS THE SERVER: after listen
+
+// connection socket for server
 int get_connection(int s) {
     int t; /* socket of connection */
 
-    if ((t = accept(s,NULL,NULL)) < 0)
-        return -1;
+    if ((t = accept(s,NULL,NULL)) < 0) {
+        printError("problem with accept");
+        exit(EXIT_FAILURE);
+    }
     return t;
 
 }
 
-//TODO THIS IS THE CLIENT: socket->connect
 //TODO who is this hostname??
+//create client socket
 int call_socket(char *hostname, unsigned short portnum) {
 
     struct sockaddr_in sa;
@@ -95,7 +101,8 @@ int call_socket(char *hostname, unsigned short portnum) {
     int s;
 
     if ((hp= gethostbyname (hostname)) == NULL) {
-        return(-1);
+        printError("problem with getting hostname in client");
+        exit(EXIT_FAILURE);
     }
 
     memset(&sa,0,sizeof(sa));
@@ -104,17 +111,17 @@ int call_socket(char *hostname, unsigned short portnum) {
     sa.sin_port = htons((u_short)portnum);
     if ((s = socket(hp->h_addrtype,
                     SOCK_STREAM,0)) < 0) {
-        return(-1);
+        printError("problem with creating client socket");
+        exit(EXIT_FAILURE);
     }
 
     if (connect(s, (struct sockaddr *)&sa , sizeof(sa)) < 0) {
         close(s);
-        return(-1);
+        printError("problem with connecting");
+        exit(EXIT_FAILURE);
     }
-
     return(s);
 }
-
 
 int read_data(int s, char *buf, int n) {
     int bcount;       /* counts bytes read */
@@ -122,7 +129,27 @@ int read_data(int s, char *buf, int n) {
     bcount= 0; br= 0;
 
     while (bcount < n) { /* loop until full buffer */
-        br = read(s, buf, n-bcount))
+        br = read(s, buf, n-bcount);
+        if ((br > 0)  {
+            bcount += br;
+            buf += br;
+        }
+        if (br < 1) {
+            printError("problem with reading");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return(bcount);
+}
+
+//TODO made a write function that is the same as the 'read_data' fuction we saw in class. do we need to do like this? write with buffer? or can we just write
+int write_data(int s, char *buf, int n) {
+    int bcount;       /* counts bytes read */
+    int br;               /* bytes read this pass */
+    bcount= 0; br= 0;
+
+    while (bcount < n) { /* loop until full buffer */
+        br = write(s, buf, n-bcount);
         if ((br > 0)  {
             bcount += br;
             buf += br;
@@ -133,21 +160,32 @@ int read_data(int s, char *buf, int n) {
     }
     return(bcount);
 }
-//TODO get last function form Nmd,
-
-
 
 int main(int argc, char *argv[]) {
     if(argc==SERVER){
-        int s;
-        int t;
-        s = establish(*argv[1]);
-        t = get_connection(s);
+        int welcome_socket;
+        int connected_socket;
+        int bcount; // number of bytes read
+        welcome_socket = establish(*argv[2]);
+        while(true){ //server waits for client connection
+            char buf[BUFFER_SIZE];
+            //TODO the connected socket connects to any client that calls it? no need to give him the calling client?
+            connected_socket = get_connection(welcome_socket);
+            //TODO write reply to connection socket - how?
+            bcount = read_data(connected_socket,buf,BUFFER_SIZE);
+            //TODO run received terminal commands from port
+        }
 
 
     }
     if(argc==CLIENT){
+        char *hostname; //TODO not sure what to do with this.
+        int client_socket;
         char* terminal_command = argv[3];
+        client_socket = call_socket(hostname,*argv[2]);
+        //TODO read reply to confirm connection - how?
+
+        write_data(client_socket,terminal_command,BUFFER_SIZE);
+
     }
-    //TODO print error number args
 }
